cd96004772c32f0018a43a371ff0c8c5424ef70e
Arek Der-Sarkissian <areksds@gmail.com>
2021-03-22 22:36:31 -0700

    Basic LinkedIn flow (commented out)

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,15 +1,16 @@
 import { useLinkProps } from '@react-navigation/native';
 import * as React from 'react';
-import { StyleSheet, Button, StatusBar } from 'react-native';
-
+import { StyleSheet, Image, Button, StatusBar } from 'react-native';
+import Layout from '../constants/Layout'
 import { connect } from 'react-redux';
 import { logIn, logOut } from '../redux/actions';
 import useColorScheme from '../hooks/useColorScheme';
 import { View, Text } from '../components/Themed';
 
+import LinkedInModal from 'react-native-linkedin'
+
 function LoginScreen(props: any) {
   const colorScheme = useColorScheme();
-
   return (
     <View style={styles.container}>
       <StatusBar
@@ -22,10 +23,25 @@ function LoginScreen(props: any) {
           props.logIn();
         }}
       />
+
+      {/* <LinkedInModal
+          clientID="86numytxpogb2d"
+          clientSecret="ZJrn5GcgOU3esZ5q"
+          redirectUri="https://dev.example.com/auth/linkedin/callback"
+          containerStyle={styles.linkedInContainer}
+          wrapperStyle={styles.linkedInWrapper}
+          onSuccess={token => console.log(token)}
+          renderButton={() => { return <Image style={styles.image} source={require('../assets/images/linkedin-login.png')} /> }}
+        /> */}
     </View>
   );
 }
 const styles = StyleSheet.create({
+  image: {
+    width: '80%',
+    height: undefined,
+    aspectRatio: 5.5,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
@@ -35,6 +51,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  linkedInContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    paddingVertical: 40,
+    paddingHorizontal: 10,
+  },
+  linkedInWrapper: {
+    flex: 1,
+    borderRadius: 5,
+    borderWidth: 10,
+    borderColor: 'rgba(255, 255, 255, 0.6)',
+  }
 });
 
 const mapDipatchToProps = {
