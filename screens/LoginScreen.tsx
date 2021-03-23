15cec895c73b9e2475e8a0d7c28ce97833a3c4a1
Arek Der-Sarkissian <areksds@gmail.com>
2021-03-23 13:39:40 -0700

    Implemented basic Auth0 flow

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,19 +1,70 @@
 import { useLinkProps } from '@react-navigation/native';
 import * as React from 'react';
-import { StyleSheet, Image, Button, StatusBar } from 'react-native';
+import { Alert, StyleSheet, Image, Button, StatusBar, Platform } from 'react-native';
 import Layout from '../constants/Layout'
 import { connect } from 'react-redux';
 import { logIn, logOut } from '../redux/actions';
 import useColorScheme from '../hooks/useColorScheme';
 import { View, Text } from '../components/Themed';
 
-// import LinkedInModal from 'react-native-linkedin'
+import * as AuthSession from 'expo-auth-session';
+import jwtDecode from 'jwt-decode';
 
-import Auth0 from 'react-native-auth0';
-const auth0 = new Auth0({ domain: 'mixr.us.auth0.com', clientId: 'BwQHBhF9SiT4jJqDUPf2nubOfF5x40r7' });
+// Auth0 Expo Code Example: https://github.com/expo/examples/tree/master/with-auth0
+
+const auth0ClientId = "BwQHBhF9SiT4jJqDUPf2nubOfF5x40r7";
+const authorizationEndpoint = "https://mixr.us.auth0.com/authorize";
+
+const useProxy = Platform.select({ web: false, default: true });
+const redirectUri = AuthSession.makeRedirectUri({ useProxy });
 
 function LoginScreen(props: any) {
   const colorScheme = useColorScheme();
+
+  const [name, setName] = React.useState(null);
+
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      redirectUri,
+      clientId: auth0ClientId,
+      // id_token will return a JWT token
+      responseType: 'id_token',
+      // retrieve the user's profile
+      scopes: ['openid', 'profile'],
+      extraParams: {
+        // ideally, this will be a random value
+        nonce: 'nonce',
+      },
+    },
+    { authorizationEndpoint }
+  );
+
+  // Retrieve the redirect URL, add this to the callback URL list
+  // of your Auth0 application.
+  console.log(`Redirect URL: ${redirectUri}`);
+
+  React.useEffect(() => {
+    if (result) {
+      if (result.type == "error") {
+        Alert.alert(
+          'Authentication error',
+          result.params.error_description || 'something went wrong'
+        );
+        return;
+      }
+      if (result.type === 'success') {
+        // Retrieve the JWT token and decode it
+        const jwtToken = result.params.id_token;
+        const decoded : any = jwtDecode(jwtToken);
+
+        const { name } = decoded;
+        setName(name);
+
+        props.logIn();
+      }
+    }
+  }, [result]);
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -23,29 +74,10 @@ function LoginScreen(props: any) {
       <Button
         title="Log in"
         onPress={() => {
-          // props.logIn();
-          auth0
-            .webAuth
-            .authorize({scope: 'openid profile email'})
-            .then(credentials =>
-              // Successfully authenticated
-              // Store the accessToken
-              // this.setState({ accessToken: credentials.accessToken })
-              console.log(credentials.accessToken)
-            )
-            .catch(error => console.log(error));
+          props.logIn();
+          // promptAsync({ useProxy }) // Uncomment to enable Auth0 login flow
         }}
       />
-
-      {/* <LinkedInModal
-          clientID="86numytxpogb2d"
-          clientSecret="ZJrn5GcgOU3esZ5q"
-          redirectUri="https://dev.example.com/auth/linkedin/callback"
-          containerStyle={styles.linkedInContainer}
-          wrapperStyle={styles.linkedInWrapper}
-          onSuccess={token => console.log(token)}
-          renderButton={() => { return <Image style={styles.image} source={require('../assets/images/linkedin-login.png')} /> }}
-        /> */}
     </View>
   );
 }
