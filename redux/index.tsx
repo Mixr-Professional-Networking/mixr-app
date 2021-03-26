80e2f4d6b329fc7965094ab8ec8f36381eaa2d2c
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-03-26 13:01:59 -0700

    First changes to redux for card style

--- redux/index.tsx
+++ redux/index.tsx
@@ -1,11 +1,17 @@
 import { combineReducers, createStore } from 'redux';
-import { updateLogin, updateMessages, updateMessageHistory } from './reducers';
+import {
+  updateLogin,
+  updateMessages,
+  updateMessageHistory,
+  cardsReducer,
+} from './reducers';
 
 const store = createStore(
   combineReducers({
     updateLogin,
     updateMessages,
     updateMessageHistory,
+    cardsReducer,
   })
 );
 
--- redux/reducers/index.tsx
+++ redux/reducers/index.tsx
@@ -1,5 +1,6 @@
 import updateLogin from './updateLogin';
 import updateMessages from './updateMessages';
 import updateMessageHistory from './updateMessageHistory';
+import cardsReducer from './cardsReducer';
 
-export { updateLogin, updateMessages, updateMessageHistory };
+export { updateLogin, updateMessages, updateMessageHistory, cardsReducer };
