19be7df80d3f5280bd58fb94f2bcc74589110bae
Arek Der-Sarkissian <areksds@gmail.com>
2021-03-27 12:40:29 -0700

    t h u n k
    npm install pls

--- redux/index.tsx
+++ redux/index.tsx
@@ -1,7 +1,10 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import AsyncStorage from '@react-native-community/async-storage';
+import thunk from 'redux-thunk';
+
+const composedEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 import {
   loginReducer,
@@ -23,7 +26,10 @@ const store = createStore(
     messages: messagesReducer,
     messageHistory: messageHistoryReducer,
     cards: cardsReducer,
-  })
+  }), 
+  composedEnhancer(applyMiddleware(
+      thunk
+  ))
 );
 const persistor = persistStore(store);
 
