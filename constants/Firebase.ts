010eaae31f3c71b6a71e0d63814f8765d097c3fc
Narek Daduryan <dadur604604@gmail.com>
2021-03-28 01:08:58 -0700

    Fix previous commit

---
+++ constants/Firebase.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKPCVSSYU8ucy-xdxlvMpf1swEqxKoF54",
+  authDomain: "mixr-ff016.firebaseapp.com",
+  projectId: "mixr-ff016",
+  storageBucket: "mixr-ff016.appspot.com",
+  messagingSenderId: "366486473981",
+  appId: "1:366486473981:web:1aa839d9557516b6470294",
+  measurementId: "G-RBW6RC8299"
+};
+
+firebase.initializeApp(firebaseConfig);
